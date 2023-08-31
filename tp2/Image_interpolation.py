413fdf42d9776b29ac8d42e6a382bb65c0808aec
Rolo2796 <48720104+R0L02796@users.noreply.github.com>
2023-08-30 23:45:58 -0300

    removed results

---
+++ tp2/Image_interpolation.py
@@ -0,0 +1,20 @@
+from PIL import Image
+
+# Abrir la imagen original
+image1 = Image.open("Rose512.jpg")
+image2 = Image.open("Rose256.jpg")
+image3 = Image.open("Rose128.jpg")
+image4 = Image.open("Rose64.jpg")
+image5 = Image.open("Rose32.jpg")
+
+images = [image1, image2, image3, image4, image5]
+
+# Definir las nuevas dimensiones deseadas para la imagen interpolada
+new_width = 1024
+new_height = 1024
+
+# Realizar la interpolación para cambiar el tamaño de la imagen
+for index, image in enumerate(images):
+    interpolated_image = image.resize((new_width, new_height), Image.NEAREST)
+    # Guardar la imagen interpolada
+    interpolated_image.save(f"interpolated_image{index}.jpg")
