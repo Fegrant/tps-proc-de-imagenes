659a41d210fbe61a9935c731e97bd44e7132a10f
Rolo2796 <48720104+R0L02796@users.noreply.github.com>
2023-09-15 21:05:24 -0300

    ej1

---
+++ tp3/Ej1/ej1.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+# Load the image
+image = cv2.imread('../image_sources/Fig0448(a)(characters_test_pattern).tif')
+sequence = [3, 5, 9, 15, 25, 35, 45, 55]
+# Apply Gaussian blur to the image
+for k in sequence:
+    k_size = (k, k)  # Kernel size, adjust as needed
+    blurred_image = cv2.GaussianBlur(image, k_size, 0)
+    cv2.imwrite(f"../results/low_pass_filtered_image{k}.jpg", blurred_image)
+
+
