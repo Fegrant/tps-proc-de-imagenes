783fbdfffc50edd80eb58fad0b2d352ff4d0be66
Fegrant <nicolasbirsa333@gmail.com>
2023-09-18 13:10:51 -0300

    added scaled images to ej4

--- tp3/Ej4/laplacian.py
+++ tp3/Ej4/laplacian.py
@@ -3,32 +3,42 @@ import numpy as np
 
 image = cv2.imread('../image_sources/blurry_moon.tif', cv2.IMREAD_GRAYSCALE)
 
-normal_mask = np.array([[0, -1, 0],
-                        [-1, 5, -1],
-                        [0, -1, 0]], dtype=np.float32)
+normal_mask = np.array([[1, 1, 1],
+                        [1, -8, 1],
+                        [1, 1, 1]], dtype=np.float32)
 
-mask_with_diagonals = np.array([[-1, -1, -1],
+borders_mask = np.array([[-1, -1, -1],
                                 [-1, 9, -1],
                                 [-1, -1, -1]], dtype=np.float32)
 
 height, width = image.shape
 normal_laplacian = np.zeros((height, width), dtype=np.float32)
-laplacian_with_diagonals = np.zeros((height, width), dtype=np.float32)
+borders_laplacian = np.zeros((height, width), dtype=np.float32)
 
 # Guardamos en una matriz la imagen con la máscara normal, y en otra con la máscara con diagonales
 for i in range(1, height - 1):
     for j in range(1, width - 1):
         region = image[i-1:i+2, j-1:j+2]
-        normal_laplacian[i, j] = np.sum(region * normal_mask)
-        laplacian_with_diagonals[i, j] = np.sum(region * mask_with_diagonals)
+        normal_value = np.sum(region * normal_mask)
+        normal_laplacian[i, j] = normal_value
+        borders_value = np.sum(region * borders_mask)
+        borders_laplacian[i, j] = borders_value
 
-normal_laplacian = np.uint8(np.absolute(normal_laplacian))
-laplacian_with_diagonals = np.uint8(np.absolute(laplacian_with_diagonals))
+normal_laplacian = np.clip(normal_laplacian, 0, 255).astype(np.uint8)
+borders_laplacian = np.clip(borders_laplacian, 0, 255).astype(np.uint8)
+# Cambio de escala al laplaciano normal, que sino satura
+normal_laplacian_scaled_down = np.clip(normal_laplacian * 0.5, 0, 255).astype(np.uint8)
+normal_laplacian_scaled_up = np.clip(normal_laplacian * 1.5, 0, 255).astype(np.uint8)
 
 image_with_normal_mask = cv2.subtract(image, normal_laplacian)
-image_with_diagonal_mask = cv2.subtract(image, laplacian_with_diagonals)
+image_with_borders_mask = cv2.subtract(image, borders_laplacian)
+image_with_normal_mask_scaled_down = cv2.subtract(image, normal_laplacian_scaled_down)
+image_with_normal_mask_scaled_up = cv2.subtract(image, normal_laplacian_scaled_up)
 
-cv2.imwrite('results/laplacian_90deg.png', normal_laplacian)
-cv2.imwrite('results/laplacian_diagonals.png', laplacian_with_diagonals)
-cv2.imwrite('results/laplacian_90deg_result.png', image_with_normal_mask)
-cv2.imwrite('results/laplacian_diagonals_result.png', image_with_diagonal_mask)
+cv2.imwrite('results/original_image.png', image)
+cv2.imwrite('results/normal_laplacian.png', normal_laplacian)
+cv2.imwrite('results/borders_laplacian.png', borders_laplacian)
+cv2.imwrite('results/image_with_normal_laplacian.png', image_with_normal_mask)
+cv2.imwrite('results/image_with_normal_laplacian_scaled_down.png', image_with_normal_mask_scaled_down)
+cv2.imwrite('results/image_with_normal_laplacian_scaled_up.png', image_with_normal_mask_scaled_up)
+cv2.imwrite('results/image_with_borders_laplacian.png', image_with_borders_mask)
