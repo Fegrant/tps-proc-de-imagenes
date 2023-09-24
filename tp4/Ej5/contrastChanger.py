f9a551f2e38bf090717953123de68d68bee72589
Rolo2796 <48720104+R0L02796@users.noreply.github.com>
2023-09-24 20:41:10 -0300

    ej5 details

--- tp4/Ej5/contrastChanger.py
+++ tp4/Ej5/contrastChanger.py
@@ -3,7 +3,8 @@ from PIL import Image, ImageEnhance
 # Open the image
 image = Image.open('../image_sources/lena_gray.tif')
 
-# Create an ImageEnhance object and adjust the contrast (factor > 1 increases contrast, factor < 1 decreases contrast)
+# Create an ImageEnhance object and adjust the contrast
+# (factor > 1 increases contrast, factor < 1 decreases contrast)
 factor = 0.5  # You can adjust this value
 enhancer = ImageEnhance.Contrast(image)
 adjusted_image = enhancer.enhance(factor)
