ae2e028e2f4fe34727ad786e8855070296c1054a
Natu <nlilienthal@itba.edu.ar>
2023-09-24 17:56:29 -0300

    add ej2 and reorganized files

---
+++ tp4/Ej2/ej2.py
@@ -0,0 +1,28 @@
+from PIL import Image, ImageFilter, ImageOps
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+image = Image.open("original_image.jpg").convert("L")
+
+# Aplica el filtro EDGE_ENHANCE para resaltar los bordes
+i_edge_enhace= image.filter(ImageFilter.EDGE_ENHANCE)
+
+i_edge_enhace_more = image.filter(ImageFilter.EDGE_ENHANCE_MORE)
+
+i_edge_enhace.save("image_filter_enhace.jpg")
+
+i_edge_enhace_more.save("image_filter_enhace_more.jpg")
+
+
+original_image = image.convert('L')
+b = np.array(original_image)
+# Create an unsharp mask kernel, high pass
+h = np.array([[0, -1, 0],
+             [-1, 5, -1],
+              [0, -1, 0]])
+
+
+# Apply the filter to the image
+i_high_pass = cv2.filter2D(b, -1, h)
+
+cv2.imwrite("high_pass.jpg", i_high_pass)
