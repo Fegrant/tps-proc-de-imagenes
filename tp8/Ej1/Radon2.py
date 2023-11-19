a5df1e8b691d58d8af02314a30651a5a861fcd8c
Natu <nlilienthal@itba.edu.ar>
2023-11-19 17:39:14 -0300

    add methods

--- tp8/Ej1/Radon2.py
+++ tp8/Ej1/Radon2.py
@@ -4,10 +4,12 @@ from skimage.transform import iradon, radon
 from skimage.data import shepp_logan_phantom
 
 phantom = shepp_logan_phantom()
+
 # Mostrar la imagen original (phantom)
 plt.imshow(phantom, cmap='gray')
 plt.title('Phantom Shepp-Logan')
 plt.show()
+
 start_angle = 0
 end_angle = 180
 angle_step = 1
@@ -22,20 +24,18 @@ plt.show()
 
 # Seleccionar parámetros para la transformada inversa de Radon
 filter_types = ['ramp', 'shepp-logan', 'cosine']
-interpolation = 'linear'  # Método de interpolación (puedes cambiarlo según tus necesidades)
+interpolation_methods = ['nearest', 'linear', 'cubic']
 
-# Crear subplots para mostrar imágenes reconstruidas con diferentes filtros
-plt.figure(figsize=(15, 5))
+# Calcular la cantidad de subplots necesarios
+num_subplots = len(filter_types) * len(interpolation_methods)
 
-for i, filter_type in enumerate(filter_types, 1):
-    # Aplicar la transformada inversa de Radon con el filtro actual
-    inverse_radon_transform = iradon(radon_transform, theta=np.arange(start_angle, end_angle, angle_step),
-                                     filter_name=filter_type, interpolation=interpolation)
+for i, filter_type in enumerate(filter_types, 1):    
+    for j, interp_method in enumerate(interpolation_methods, 1):
+        # Aplicar la transformada inversa de Radon con el filtro actual
+        inverse_radon_transform = iradon(radon_transform, theta=np.arange(start_angle, end_angle, angle_step),
+                                         filter_name=filter_type, interpolation=interp_method)
 
-    # Mostrar la imagen reconstruida en un subplot
-    plt.subplot(1, len(filter_types), i)
-    plt.imshow(inverse_radon_transform, cmap='gray')
-    plt.title(f'Transformada inversa de Radon\nFiltro: {filter_type}')
-
-plt.tight_layout()
-plt.show()
+        # Mostrar cada imagen por separado
+        plt.imshow(inverse_radon_transform, cmap='gray', vmin=phantom.min(), vmax=phantom.max())  # Escala consistente
+        plt.title(f'Interpolación: {interp_method} - Filtro: {filter_type}')
+        plt.show()
